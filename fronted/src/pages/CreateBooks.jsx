543af8955578aaecebdecd9045467a31fbee73a8
CSERiya <93813209+CSERiya@users.noreply.github.com>
2023-12-28 20:13:52 +0530

    added several components

--- fronted/src/pages/CreateBooks.jsx
+++ fronted/src/pages/CreateBooks.jsx
@@ -3,7 +3,7 @@ import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 import axios from'axios';
 import { useNavigate } from 'react-router-dom';
-
+import { useSnackbar } from 'notistack';
 
 const CreateBooks = () => {
   const[title, setTitle]=useState('');
@@ -11,6 +11,8 @@ const CreateBooks = () => {
   const[publishYear, setPublishYear]=useState('');
   const[loading, setLoading]=useState(false);
   const navigate=useNavigate();
+  const {enqueueSnackbar}=useSnackbar();
+
   const handleSaveBook=()=>{
 const data={
   title,
@@ -21,11 +23,13 @@ setLoading(true);
 axios.post('http://localhost:5555/books',data)
 .then(()=>{
   setLoading(false);
+  enqueueSnackbar('Book created successfully', {variant:'success'})
   navigate('/');
 })
 .catch((error)=>{
   setLoading(false);
-  alert('An error happened. Please Check console');
+  // alert('An error happened. Please Check console');
+  enqueueSnackbar('Error', {variant:'error'});
   console.log(error);
 });
   };
@@ -57,7 +61,7 @@ axios.post('http://localhost:5555/books',data)
         <div className='my-4'>
           <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
           <input
-          type='text'
+          type='number'
           value={publishYear}
           onChange={(e)=>setPublishYear(e.target.value)}
           className='border-2 border-gray-500 px-4 py-2 w-full'
